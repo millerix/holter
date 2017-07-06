91fd1a4cca0ca12ea500aeba348a4c4d1d5d00dc
ljbj <ljbj@LJBJ-PC>
2017-07-06 21:42:21 +0800

    c6

--- Holter_vs2015/config_manager.cpp
+++ Holter_vs2015/config_manager.cpp
@@ -2,21 +2,34 @@
 #include "cJSON.h"
 #include <iostream>
 #include <sstream>
+#ifdef _DEBUG_
+#include <afxwin.h>
+#endif
+
 using namespace std;
 
 config_manager::config_manager()
 {
+
 	configpath="F:\\holter.conf";
-	try {
-		fconfig.open(configpath, ifstream::in);
-	}
-	catch (exception &e) {
-	//TRACE()
-	//	TRACE(e.what());
-		exit(-1);
-	}
+
+	fconfig.open(configpath, ifstream::in);
+	
 	if (fconfig.is_open()) {
 		parse();
+	}
+	else {
+#ifdef _DEBUG_
+		string errmsg = "file open failed, using default value";
+		AfxMessageBox(errmsg.c_str());
+#endif
+		server_addr = "http://172.104.78.181";
+		ftp_addr = "ftp://172.104.78.181:2121";
+		ftp_id = "admin";
+		ftp_psw = "admin";
+		login_rest = "http://172.104.78.181/client/login.do";
+		upload_rest = "http://172.104.78.181/case/postcase.do";
+		download_rest = "";
 	}
 }
 void config_manager::parse()
